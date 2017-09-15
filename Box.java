8edc38738a332c1df58a7a726541049e828f7b53
ethanmchung <echung2018@pingry.org>
2017-09-15 08:03:52 -0400

    finished assignment

--- Box.java
+++ Box.java
@@ -1,62 +1,111 @@
-public class Box {
+// The <T> is a generic type
+public class Box <T>
+{
 
   private T contents;
   private boolean isFull;
 
   // Make a new empty box
-  public Box(){
+  public Box()
+  {
+	super();
+	this.isFull = false;
   }
 
   // Make a new box pre-filled with contents
-  public Box(){
+  public Box(T contents)
+  {
+  	super();
+  	this.isFull = true;
+  	this.contents = contents;
   }
 
   /**
    * Check the contents of the box
+   * @return whatever is in the box
    */
-  public getContents(){
+  public T getContents()
+  {
+  	return contents;
   }
 
   /**
    * Tell whether the box is full or not
+   * @return whether box is full
    */
-  public isFull(){
+  public boolean isFull()
+  {
+  	return isFull;
   }
 
   /**
-   * Empty out the box, and give back hatever was in it
+   * Empty out the box, and give back whatever was in it
+   * @return the contents of the box
    */
-  public empty(){
+  public T empty()
+  {
+  	T junk = contents;
+  	contents = null;
+  	isFull = false;
+  	return junk;
+  }
+ 
+  /** 
+   * Returns a String of info about the box
+   * @return the String of the contents
+   */ 
+  public String toString()
+  {
+  	if (isFull)
+  		return contents.toString();
+  	else
+  		return "The box is not full";
+  }
+  
+  /**
+   * method adds an item to the box
+   * @param contents to be added to the box 
+   * @return if the item was successfully added
+   */
+  public boolean addItem(T contents)
+  {
+	  if(!isFull)
+	  {
+		  this.contents = contents;
+		  isFull = true;
+		  return true;
+	  }
+	  return false;
   }
 
-  public String toString(){
-  }
-
-  public static void main(String[] args){
+  /**
+   * main method which initializes boxes to be used when testing the previous methods
+   * @param args  Command line arguments
+   */
+  public static void main(String[] args)
+  {
 
     // Make two boxes
     Box<String> stringBox = new Box<String>();
-    //TODO start the second box with contents inside
-    Box<Integer> intBox   = new Box<Integer>();
+    // start the second box with contents inside
+    Box<Integer> intBox   = new Box<Integer>(10);
 
     // View contents (via toString method)
     System.out.println("The boxes contain: " + stringBox + ", " + intBox);
 
-    //TODO Add something to a box
-
-
-
+	//test addItem method
+    stringBox.addItem("compsci");
+	
     // View contents (via toString method)
     System.out.println("The boxes contain: " + stringBox + ", " + intBox);
 
-    //TODO Test your peek method
+   	//"peek" method?
+    // View contents
+    System.out.println("The boxes contain: " + stringBox.getContents() + ", " + intBox.getContents());
 
-
-    // View contents (via toString method)
-    System.out.println("The boxes contain: " + stringBox + ", " + intBox);
-
-    //TODO Empty a boxes
-
+    // Empty boxes
+	stringBox.empty();
+    intBox.empty();
 
 
     // View contents (via toString method)
